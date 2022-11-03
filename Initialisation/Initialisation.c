24c7ad624f3c345e404e256ea8a6032be31fd5f5
PhileasR <phileas.rousselet@edu.ece.fr>
2022-11-03 15:15:31 +0100

    Debut sans erreur dimmensions

--- Initialisation/Initialisation.c
+++ Initialisation/Initialisation.c
@@ -3,9 +3,13 @@
 void initCases(Case** matriceCases){
 
     for (int i = 0; i < NB_LIGNES; i++) {
-        for (int j = 0; j  < NB_COLONES; j++) {
-            matriceCases[i][j].x = 62 + 20*j;
+        for (int j = 0; j  < NB_COLONNES; j++) {
+            matriceCases[i][j].x = (62 + 20*j);
             matriceCases[i][j].y = 34 + 20*i;
+            matriceCases[i][j].pHabitation = NULL;
+            matriceCases[i][j].pChateau = NULL;
+            matriceCases[i][j].pCentrale = NULL;
+            matriceCases[i][j].type = -1;
         }
     }
 }
