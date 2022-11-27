6b2a303e681ca63636f23816a611794517db04f5
PhileasR <phileas.rousselet@edu.ece.fr>
2022-11-27 22:27:47 +0100

    Version Finale

--- Ecrans accueil/Ecrans accueil.h
+++ Ecrans accueil/Ecrans accueil.h
@@ -1,17 +1,8 @@
 #ifndef ECE_CITY_ECRANS_ACCUEIL_H
 #define ECE_CITY_ECRANS_ACCUEIL_H
-#include <stdio.h>
-#include <allegro5/allegro.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <assert.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-#include "allegro5/allegro_primitives.h"
-#include "../Initialisation/Initialisation.h"
 
+#include "../Initialisation/Initialisation.h"
+#include "../Fonctions/Fonctions.h"
 
 void ecranAccueil(ALLEGRO_DISPLAY* fenetre);
 
